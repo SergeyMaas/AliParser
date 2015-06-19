68651115bb7eec614079359997a63e8f2fef4b03
Vladimir Nikolaenko <vladimir.nikolaenko@firstlinesoftware.com>
2015-06-19 16:23:18 +0300

    Simple match

--- src/main/java/org/nick/utils/App.java
+++ src/main/java/org/nick/utils/App.java
@@ -7,56 +7,89 @@ import com.ui4j.api.dom.Element;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Hello world!
  */
 public class App {
+    static BrowserEngine browser = BrowserFactory.getWebKit();
+
     public static void main(String[] args) throws IOException {
+        final List<Map<String, String>> mouseMap = getStoreMap("bluetooth", "mouse", "-2.4Ghz", "-keyboard");
+        final List<Map<String, String>> chargerMap = getStoreMap("3 port usb car charger");
 
-
-        final String url = "http://www.aliexpress.com/wholesale?initiative_id=SB_20150617021423&site=glo&shipCountry=ru&SearchText=bluetooth+mouse+-2.4Ghz+-keyboard&page=1";
-        /*
-        final HtmlCleaner htmlCleaner = new HtmlCleaner();
-        final TagNode html = htmlCleaner.clean(new URL(url));
-        final TagNode hslist = html.getElementsByAttValue("id", "hs-list-items", true, true)[0];
-        final TagNode blist = html.getElementsByAttValue("id", "hs-below-list-items", true, true)[0];
-
-        for (TagNode li : hslist.getElementsByName("li", false)) {
-            System.out.println(li);
+        final Map<Map<String, String>, Map<String, String>> matches = new HashMap<>();
+        for (Map<String, String> mouse : mouseMap) {
+            for (Map<String, String> charger : chargerMap) {
+                if (mouse.get("storeLink").equals(charger.get("storeLink"))) {
+                    matches.put(mouse, charger);
+                }
+            }
         }
 
-        for (TagNode li : blist.getElementsByName("li", true)) {
-            System.out.println(li);
-        }*/
+        matches.toString();
+    }
 
-        //WebView view = new WebView();
+    public static List<Map<String, String>> getStoreMap(final String... query) {
+        final StringBuilder querySB = new StringBuilder();
 
+        for (String s : query) {
+            if (s != null) {
+                if (querySB.length() > 0) {
+                    querySB.append("+");
+                }
+                querySB.append(s.replaceAll(" ", "+"));
+            }
+        }
 
-        // get the instance of the webkit
+        final String url = "http://www.aliexpress.com/wholesale?shipCountry=ru&page=1&groupsort=1&isFreeShip=y&SortType=total_tranpro_desc&SearchText=" + querySB.toString();
+
         BrowserEngine browser = BrowserFactory.getWebKit();
 
-        final Page page = browser.navigate(url);
+        /*PageConfiguration config = new PageConfiguration(new Interceptor() {
 
-        final Map<String, String> links = new HashMap<>();
+            @Override
+            public void beforeLoad(Request request) {
+                request.setCookies(Arrays.asList(
+                                new HttpCookie("intl_locale", "en_US"),
+                                new HttpCookie("Cookie2", "Value2"))
+                );
+            }
+
+            @Override
+            public void afterLoad(Response response) {
+
+            }
+        });*/
+
+        Page page = browser.navigate(url);
+        final List<Map<String, String>> links = new LinkedList<>();
         for (Element ul : page.getDocument().queryAll("ul")) {
             for (Element listItem : ul.queryAll(".list-item")) {
-                String link = null;
-                String store = null;
+                final Map<String, String> item = new HashMap<>();
 
                 for (Element a : listItem.queryAll("a")) {
                     final String href = a.getAttribute("href");
                     if (href.toLowerCase().contains("/store") && !href.toLowerCase().contains("/feedback")) {
-                        store = href;
+                        item.put("storeLink", href);
+                        item.put("storeTitle", a.getText());
                     }
                     if (href.toLowerCase().contains("item") && href.toLowerCase().endsWith(".html")) {
-                        link = href;
+                        item.put("item", href);
+                    }
+                    if (href.toLowerCase().contains("item") && href.toLowerCase().endsWith("#thf")) {
+                        final String em = a.query("em").getText();
+                        final String ordersString = em.substring(em.indexOf('(') + 1, em.indexOf(')'));
+                        if (Integer.parseInt(ordersString) > 0) {
+                            item.put("orders", ordersString);
+                        }
                     }
 
-
-                    if (link != null && store != null) {
-                        links.put(store, link);
+                    if (item.size() == 4) {
+                        links.add(item);
                         break;
                     }
                 }
@@ -64,6 +97,7 @@ public class App {
         }
 
         page.close();
-        browser.shutdown();
+
+        return links;
     }
 }
